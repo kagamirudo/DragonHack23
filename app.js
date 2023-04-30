75c54d88258836586b37ad0d2777e61158b8d96f
1emb <minhkhac.pro@gmail.com>
2023-04-29 20:12:29 -0400

    confusion deleted

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-async function gg(){
+async function gg(prompt = "Hello world"){
   const { Configuration, OpenAIApi } = require("openai");
 
   const configuration = new Configuration({
@@ -13,17 +13,24 @@ async function gg(){
 
   const completion = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: "Hello world",
+    prompt: prompt,
   });
   console.log(completion.data.choices[0].text);
   
 }
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-  gg();
+  res.sendFile( __dirname + "/" + "index.html" );
+  
 })
 
-
+app.get('/process_get', function (req, res) {
+  // Prepare output in JSON format
+  response = {
+    ingredients:req.query.ingredients    
+  };
+  console.log(response);
+  res.end(JSON.stringify(response));
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on http://localhost:${port}`)
