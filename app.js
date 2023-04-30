4f2a2ad5f6ad8069f78f53f3ccecdf8aed3b7144
Gary Pham <gp492@drexel.edu>
2023-04-29 20:06:02 -0400

    hhhhh

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 const key = require('./authen.js')
->>>>>>> 97317e051e89bc4def698f225fa4da2f7a62a486
 const express = require('express')
 const app = express()
 const port = 3000
@@ -9,9 +6,8 @@ const port = 3000
 async function gg() {
     const { Configuration, OpenAIApi } = require("openai");
 
-<<<<<<< HEAD
     const configuration = new Configuration({
-        apiKey: "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",
+        apiKey: key,
     });
     const openai = new OpenAIApi(configuration);
 
@@ -20,19 +16,7 @@ async function gg() {
         prompt: "Hello world",
     });
     console.log(completion.data.choices[0].text);
-=======
-  const configuration = new Configuration({
-    apiKey: key,
-  });
-  const openai = new OpenAIApi(configuration);
 
-  const completion = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: "Hello world",
-  });
-  console.log(completion.data.choices[0].text);
-  
->>>>>>> 97317e051e89bc4def698f225fa4da2f7a62a486
 }
 app.get('/', (req, res) => {
     res.send('Hello World!')
